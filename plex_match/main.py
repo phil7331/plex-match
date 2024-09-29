c156b3e90aa0fe30957fa09cf62c1e5f97cb6bf3
Philipp Flügger <philipp.fluegger@dfki.de>
2024-09-29 19:00:42 +0200

    Fix request error

--- plex_match/main.py
+++ plex_match/main.py
@@ -3,16 +3,19 @@ import requests
 
 app = Flask(__name__)
 
+
 # Define the route for the homepage
 @app.route('/')
 def index():
     # Replace this with the API you're calling
-    api_url = 'https://api.example.com/data'
-    response = requests.get(api_url)
-    data = response.json()
+    # api_url = 'https://api.example.com/data'
+    # response = requests.get(api_url)
+    context = [1, 2, 3]
 
     # Pass the data to the frontend
-    return render_template('index.html', data=data)
+    return render_template(template_name_or_list='index.html', context=context)
+
 
 if __name__ == '__main__':
+    print("HELLO")
     app.run(debug=True)
