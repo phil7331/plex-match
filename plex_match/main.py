96c0d186cf4044d1b9be5f41e477b4dccd759423
Philipp Flügger <philipp.fluegger@dfki.de>
2024-09-30 20:48:19 +0200

    Update backend routes

--- plex_match/main.py
+++ plex_match/main.py
@@ -1,36 +1,68 @@
-from flask import Flask, render_template
-import requests
-from dotenv import load_dotenv
+from flask import Flask, render_template, jsonify, request, redirect, make_response
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import os
+from dotenv import load_dotenv
 from plexapi.server import PlexServer
-import pymongo
 
+from plex_match.repository import init_admin_user, find_user_in_db
 
 load_dotenv()
 app = Flask(__name__)
 
+# JWT configuration
+app.config['JWT_SECRET_KEY'] = 'ea32dbef1a435a4542da8a2994619bda1b4a01cc17e6dbfa4ef0798f6976fbac'
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']  # Store JWT in cookies
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False  # Disable CSRF protection for simplicity
+jwt = JWTManager(app)
 
-# Define the route for the homepage
+users = {"asd": {"password": "asd"}}
+
+
+@jwt.unauthorized_loader
+def unauthorized_response(callback):
+    return redirect('/login', code=302)
+
+
+@jwt.expired_token_loader
+def expired_token_response(jwt_header, jwt_payload):
+    return redirect('/login', code=302)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = find_user_in_db(username=username, password=password)
+
+        # Create JWT and store it in a cookie
+        access_token = create_access_token(identity=user["username"])
+        resp = make_response(redirect('/'))
+        resp.set_cookie('access_token_cookie', access_token)
+
+        return resp
+
+    return render_template('login.html')
+
+
 @app.route('/')
-def index():
-    # Replace this with the API you're calling
-    # api_url = 'https://api.example.com/data'
-    # response = requests.get(api_url)
-    context = [1, 2, 3]
-
-    # Pass the data to the frontend
-    return render_template(template_name_or_list='index.html', context=context)
-
-
-@app.route('/match')
+@jwt_required()  # JWT will be taken from the cookie automatically
 def match():
     token = os.getenv("TOKEN")
     url = os.getenv("URL")
     plex = PlexServer(url, token)
     movies = plex.library.section("Movies").search(unwatched=True)
-    print()
+
+    movies_posters = []
+    for movie in movies:
+        movie_title = movie.title
+        movies_posters.append({"title": movie_title, "poster_url": movie.posterUrl})
+    current_user = get_jwt_identity()
+    return render_template('index.html', context=movies_posters)
 
 
 if __name__ == '__main__':
-    print("HELLO")
+    if False:
+        init_admin_user()
     app.run(debug=True)
